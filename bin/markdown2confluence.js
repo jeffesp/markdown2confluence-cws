58f75ead4e8655cafc8ebfdfa91db7116c6b028e
Jeff Espenschied <jeffesp@nexosis.com>
2017-04-07 15:05:36 -0400

    Make it read a config file to change the output.

--- bin/markdown2confluence.js
+++ bin/markdown2confluence.js
@@ -2,13 +2,20 @@
 /* eslint no-process-exit:off */
 "use strict";
 
-var filename, fs, getStdin, markdown2confluence, path;
+var filename, fs, getStdin, markdown2confluence, path, os, config, configFile;
 
 getStdin = require("get-stdin");
 markdown2confluence = require("../");
 fs = require("fs");
 path = require("path");
+os = require("os");
+
 filename = process.argv[2];
+
+configFile = path.join(os.homedir(), ".config", "markdown2confluence", "config.json")
+if (fs.existsSync(configFile)) {
+    config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+}
 
 if (filename && filename !== "-") {
     filename = path.resolve(process.cwd(), filename);
@@ -17,11 +24,11 @@ if (filename && filename !== "-") {
             console.error(`Error reading file: ${filename}`);
             process.exit(1);
         } else {
-            console.log(markdown2confluence(buf.toString("utf8")));
+            console.log(markdown2confluence(buf.toString("utf8"), config));
         }
     });
 } else {
     getStdin().then((str) => {
-        console.log(markdown2confluence(str));
+        console.log(markdown2confluence(str, config));
     });
 }
